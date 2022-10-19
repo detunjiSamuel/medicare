72c230e5565678beff4317d216b297b6b5a1e8f7
Adetunji Samuel <samadetunji01@gmail.com>
2022-10-19 13:08:16 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,35 @@
+export const seo = {
+  title: "Portfolio Boilerplate",
+  description:
+    "A Next.js boilerplate for building your portfolio as quick as possible",
+  canonical: "https://portfolio-boilerplate-nextjs.vercel.app/",
+};
+
+export const data = [
+  {
+    title: "Pharmaceutical and Research Companies",
+    description:
+    "You no longer need to approach hospitals or clinics for information. Through Medicalchain you can access a database of patients who have opted in to being contacted by researchers. The information you receive is up to date, accurate and streamlined to your specific enquiries. Following up patients is simplified with a dynamic health record that stays with the patient. ",
+    image: "/code-review-bro.png",
+  },
+  {
+    title: "Developers and Monetising Health Records",
+    description:
+    "Get in touch with us to find out more about the monetary value that health data holds. We’re at the cutting edge of health technology and we’re excited to work with other trailblazers and early adopters who will help us to shape the future of digital healthcare.",
+    image: "/code-review-bro.png",
+  },
+  {
+    title: "Patients at the center",
+    description:
+    "Patients have the ability to grant access to their electronic health records (EHR) to other users and revoke access by setting up a time limited gateway, thereby improving their experience and guaranteeing data security.",
+    image: "/code-review-bro.png",
+  },
+  {
+    title: "Medical insurance",
+    description:
+    "Cut out the middle-man and receive prompt, validated health information directly from patients allowing you to access their accurate and up to date record in more cost and time effective manor.",
+    image: "/code-review-bro.png",
+  },
+];
+
+export const MAX_WIDTH = "1440px";
